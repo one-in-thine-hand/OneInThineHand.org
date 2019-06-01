360d00389ad4de24b77f6e0b3bd3f0bf1627b23c
Jared Parkinson <jared@parkinson.im>
2019-05-31 23:03:09 -0600

    init

---
+++ format-tags/src/format-tags.ts
@@ -0,0 +1,11 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
+export default class FormatTags {
+  /**
+   * main
+   */
+  public async main(document: Document): Promise<[]> {
+    return []
+  }
+}
