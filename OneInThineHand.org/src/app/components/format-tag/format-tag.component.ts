00d4048155809c8962ff73ba8b820abfb36410e9
Jared Parkinson <jared@parkinson.im>
2019-06-08 05:02:35 -0600

    Adding text formating

--- OneInThineHand.org/src/app/components/format-tag/format-tag.component.ts
+++ OneInThineHand.org/src/app/components/format-tag/format-tag.component.ts
@@ -5,6 +5,7 @@ import {
   formatTagTypeOptions,
   DisplayAs,
 } from '../../../../../shared/src/shared';
+import { MarkService } from '../../services/mark.service';
 
 @Component({
   selector: 'app-format-tag',
@@ -13,27 +14,31 @@ import {
 })
 export class FormatTagComponent implements OnInit {
   @Input() public fMerged: FMerged;
-  public constructor() {}
+  public constructor(public markService: MarkService) {}
 
   public ngOnInit(): void {}
 
   public getText(): string {
     const text = this.fMerged.text;
     if (this.fMerged.formatTags && this.fMerged.formatTags.length > 0) {
-      this.fMerged.formatTags
-        .filter(
-          (f): boolean => {
-            return (
-              f.displayAs === DisplayAs.RICHTEXT &&
-              f.optional !== Optional.NEVER
-            );
-          },
-        )
-        .map(
-          (f): void => {
-            console.log(f);
-          },
-        );
+      const richFormatTags = this.fMerged.formatTags.filter(
+        (f): boolean => {
+          return (
+            f.displayAs === DisplayAs.RICHTEXT && f.optional !== Optional.NEVER
+          );
+        },
+      );
+      // .map(
+      //   (f): void => {
+      //     console.log(f);
+      //   },
+      // );
+      // console.log('oiasjdfoiajsdfoiasjdfoi');
+
+      return this.markService.getFormatTagRichText(
+        this.fMerged.text,
+        richFormatTags,
+      );
       return text;
     }
     return text;
---
+++ OneInThineHand.org/src/app/services/mark.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import * as MarkdownIt from 'markdown-it';
+import { uniq } from 'lodash';
+import { FormatTag, FormatTagType } from '../../../../shared/src/shared';
+@Injectable({
+  providedIn: 'root',
+})
+export class MarkService {
+  private markdownIt = new MarkdownIt({
+    html: true,
+  });
+  public constructor() {}
+
+  public getFormatTagRichText(
+    text: string | undefined,
+    formatTags: FormatTag[] | undefined,
+  ): string {
+    if (text && formatTags) {
+      const markDownText = this.getCommonMark(formatTags);
+
+      let outputText = this.markdownIt.render(
+        `${markDownText.preText}<f>${text}</f>${markDownText.postText}`,
+      );
+      ['<f>', '</f>', '<p>', '</p>'].map(
+        (replace): void => {
+          outputText = outputText.replace(replace, '');
+        },
+      );
+      console.log(outputText);
+
+      return outputText;
+    } else {
+      return text ? text : '';
+    }
+  }
+
+  private getCommonMark(
+    text: FormatTag[],
+  ): { preText: string; postText: string } {
+    const commonMark: { preText: string; postText: string } = {
+      preText: '',
+      postText: '',
+    };
+    uniq(text).map(
+      (rich): void => {
+        let addedText = '';
+        switch (rich.formatType) {
+          case FormatTagType.verseNumber: {
+            addedText = '**';
+            break;
+          }
+          case FormatTagType.italic: {
+            addedText = '_';
+            break;
+          }
+        }
+        commonMark.preText = `${addedText}${commonMark.preText}`;
+        commonMark.postText = `${commonMark.postText}${addedText}`;
+      },
+    );
+
+    return commonMark;
+  }
+}
