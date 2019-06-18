dcf467c5fecbfbd81b8b743a8c861394f0d1b6bc
Jared Parkinson <jared@parkinson.im>
2019-06-18 05:38:16 -0600

    More layout work

--- OneInThineHand.org/src/app/components/title-header/title-header.component.ts
+++ OneInThineHand.org/src/app/components/title-header/title-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderService } from '../../services/header.service';
+import { SaveStateService } from '../../services/save-state.service';
 
 @Component({
   selector: 'app-title-header',
@@ -7,6 +8,24 @@ import { HeaderService } from '../../services/header.service';
   styleUrls: ['./title-header.component.scss'],
 })
 export class TitleHeaderComponent implements OnInit {
-  public constructor(public headerService: HeaderService) {}
-  public ngOnInit() {}
+  public constructor(
+    public headerService: HeaderService,
+    public saveStateService: SaveStateService,
+  ) {}
+  public ngOnInit(): void {}
+
+  public async navigationPaneToggle(): Promise<void> {
+    this.saveStateService.data.navigationPaneToggle = !this.saveStateService
+      .data.navigationPaneToggle;
+    await this.saveStateService.save();
+  }
+
+  public async notesPaneToggle(): Promise<void> {
+    this.saveStateService.data.notesPaneToggle = !this.saveStateService.data
+      .notesPaneToggle;
+
+    await this.saveStateService.save();
+  }
+
+  // public
 }
