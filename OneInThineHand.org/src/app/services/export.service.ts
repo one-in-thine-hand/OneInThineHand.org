bd58c66cd16dc5656a8ce796c3034e996b887889
Jared Parkinson <jared@parkinson.im>
2019-06-20 10:01:47 -0600

    Export almost works

--- OneInThineHand.org/src/app/services/export.service.ts
+++ OneInThineHand.org/src/app/services/export.service.ts
@@ -2,8 +2,14 @@ import { Injectable } from '@angular/core';
 import { sortBy } from 'lodash';
 import { DatabaseService } from './database.service';
 import { ChapterService } from './chapter.service';
-import { CouchDoc, Note } from '../../../../shared/src/shared';
+import {
+  CouchDoc,
+  Note,
+  getReferenceLabelByNoteCategory,
+  ReferenceLabel,
+} from '../../../../shared/src/shared';
 import { ChapterNotes } from '../../../../notes/src/main';
+import { saveAs } from 'file-saver';
 
 @Injectable({
   providedIn: 'root',
@@ -25,12 +31,14 @@ export class ExportService {
       if (docs) {
         const ids = sortBy(
           docs.rows
-            .filter((d): boolean => {
-              return (
-                d.id.includes(`${idSplit[0]}-${idSplit[1]}`) &&
-                d.id.includes('note')
-              );
-            })
+            .filter(
+              (d): boolean => {
+                return (
+                  d.id.includes(`${idSplit[0]}-${idSplit[1]}`) &&
+                  d.id.includes('note')
+                );
+              },
+            )
             .map(
               (d): CouchDoc => {
                 return { id: d.id, rev: d.value.rev };
@@ -43,40 +51,78 @@ export class ExportService {
 
         const bulkGetDocs = await this.databaseService.bulkGet(ids);
         if (bulkGetDocs) {
-          const exportText = bulkGetDocs.results
+          const exportText = `<?xml version="1.0" encoding="UTF-8"?>\n${bulkGetDocs.results
             .map(
               (r): ChapterNotes => {
                 return (r.docs[0] as any).ok as ChapterNotes;
               },
             )
-            .map((cNotes): string => {
-              return this.chapterNotesToString(cNotes);
-            })
-            .join('\n');
+            .map(
+              (cNotes): string => {
+                return this.chapterNotesToString(cNotes);
+              },
+            )
+            .join('\n')}`;
+
+          const blob = new Blob([exportText], {
+            type: 'text/html;charset=utf=8',
+          });
+          saveAs(blob, 'test.html');
+          console.log(blob);
           console.log(exportText);
         }
       }
     }
   }
   public chapterNotesToString(cNotes: ChapterNotes): string {
-    let chapterNotesString = '<xml>';
+    let chapterNotesString = `<chapter id="${cNotes._id}">`;
     if (cNotes.notes) {
-      chapterNotesString = `${chapterNotesString}${cNotes.notes
-        .map((verseNote): string => {
-          return `${this.notesToString(verseNote.notes)}`;
-        })
-        .join('')}`;
+      chapterNotesString = `${chapterNotesString} ${cNotes.notes
+        .map(
+          (verseNote): string => {
+            return `<verse-notes id="${verseNote._id}">${this.notesToString(
+              verseNote.notes,
+            )}</verse-notes>`;
+          },
+        )
+        .join('')}</chapter>`;
     }
     return chapterNotesString;
   }
   public notesToString(notes: Note[] | undefined): string {
     if (notes) {
       return notes
-        .map((note): string => {
-          return `<note id="${note.id}">
-          <note-title>${note.id}</note-title>
+        .map(
+          (note): string => {
+            return `<note${
+              note.classList !== undefined
+                ? `class="${note.classList.join(' ')}"`
+                : ''
+            } id="${note.id}">
+          <p class="note-phrase">${
+            note.notePhrase ? note.notePhrase.text : ''
+          }</p>
+          ${note.noteRefs
+            .map(
+              (noteRef): string => {
+                let refLabel: ReferenceLabel | undefined;
+                if (noteRef.noteCategory) {
+                  refLabel = getReferenceLabelByNoteCategory(
+                    noteRef.noteCategory,
+                  );
+                }
+
+                return `<p class="note-reference"><span class="${
+                  refLabel ? refLabel.className : ''
+                }">${refLabel ? refLabel.referenceLabelShortName : ''} </span>${
+                  noteRef.text
+                }</p>`;
+              },
+            )
+            .join('\n')}
           </note>`;
-        })
+          },
+        )
         .join('\n');
     }
     return '';
