332dae119634923630221f79ef783e53a3e69811
Jared Parkinson <jared@parkinson.im>
2019-07-03 00:54:43 -0600

    New harmony format

--- harmony/src/Harmony.ts
+++ harmony/src/Harmony.ts
@@ -1,27 +1,45 @@
 import { Verse } from '../../shared/src/shared';
 
-export abstract class HarmonyElement {
-  public verseTitle: string;
-  public verseRef: string;
-}
+// export abstract class HarmonyElement {
+//   public verseTitle: string;
+//   public verseRef: string;
+// }
 
-export class HarmonyVerse extends HarmonyElement {
-  public verse: Verse;
-}
-export class HarmonyXRef {
-  public xRefTitle: string;
-  public harmonyVerse: HarmonyVerse[] | undefined;
-}
+// export class HarmonyVerse extends HarmonyElement {
+//   public verse: Verse;
+// }
+// export class HarmonyXRef {
+//   public xRefTitle: string;
+//   public harmonyVerse: HarmonyVerse[] | undefined;
+// }
+
+// export class HarmonyCell {
+//   public sortKey: string | undefined;
+//   public harmonyVerses: HarmonyVerse[] | undefined;
+//   public harmonyXRef: HarmonyXRef[] | undefined;
+//   public sequenceTitle: string | undefined;
+// }
+// export class Harmony {
+//   public language: string;
+//   public _id: string;
+//   public _rev: string | undefined;
+//   public harmonyCells: (HarmonyCell | HarmonyXRef)[][];
+// }
 
 export class HarmonyCell {
-  public sortKey: string | undefined;
-  public harmonyVerses: HarmonyVerse[] | undefined;
-  public harmonyXRef: HarmonyXRef[] | undefined;
-  public sequenceTitle: string | undefined;
+  public sortKey: string;
+  public verseRef: string | undefined;
+  public verse: Verse | undefined;
 }
+
+export class HarmonyRow {
+  public harmonyCells: HarmonyCell[];
+}
+
 export class Harmony {
-  public language: string;
   public _id: string;
   public _rev: string | undefined;
-  public harmonyCells: (HarmonyCell | HarmonyXRef)[][];
+  public harmonyRows: HarmonyRow[];
+  public title: string | undefined;
+  public titleNumber: string | undefined;
 }
--- harmony/src/main.ts
+++ harmony/src/main.ts
@@ -1,231 +1,91 @@
 import FastGlob from 'fast-glob';
-import { readFile, writeFile, pathExists, mkdirp } from 'fs-extra';
+import { readFile, mkdirp } from 'fs-extra';
 import { JSDOM } from 'jsdom';
-import { HarmonyCell, HarmonyXRef, HarmonyVerse, Harmony } from './Harmony';
-import { normalize, basename } from 'path';
+import { writeFile } from 'fs-extra';
+import { normalize } from 'path';
+import { HarmonyCell, HarmonyRow, Harmony } from './Harmony';
 export async function getHarmoniesFileNames(): Promise<string[]> {
   return FastGlob('../scripture_files/harmony/**/**', {
     onlyFiles: true,
   });
 }
 
-function calculateColumnLength(rows: Element[]): number {
-  const lengthMap: Map<number, number> = new Map();
-  rows.map(
-    (row): void => {
-      const tdsLength = row.querySelectorAll('td').length;
+function getTextContent(
+  element: Element,
+  selector: string,
+): string | undefined {
+  const textElement = element.querySelector(selector);
+  return textElement && textElement.textContent
+    ? textElement.textContent
+    : undefined;
+}
+function getHarmonyCell(element: Element): HarmonyCell {
+  const sortKey = getTextContent(element, 'sortKey');
+  const verseRef = element.querySelector('verseRef');
 
-      let length = lengthMap.get(tdsLength);
+  if (sortKey) {
+    return {
+      verse: undefined,
+      sortKey: sortKey,
+      verseRef: verseRef ? verseRef.id : undefined,
+    };
+  }
 
-      if (!length) {
-        length = 0;
-      }
+  throw '';
+}
 
-      lengthMap.set(tdsLength, length + 1);
+function getHarmonyRow(harmony: Element): HarmonyRow[] {
+  return Array.from(harmony.querySelectorAll('table tr')).map(
+    (row): HarmonyRow => {
+      return {
+        harmonyCells: Array.from(row.querySelectorAll('td')).map(
+          (col): HarmonyCell => {
+            return getHarmonyCell(col);
+          },
+        ),
+      };
     },
   );
-  // console.log(lengthMap);
-
-  return Array.from(lengthMap).sort(
-    (a, b): number => {
-      return b[1] - a[1];
-    },
-  )[0][0];
 }
+function getHarmonyChapters(document: Document): Harmony[] {
+  return Array.from(document.querySelectorAll('harmony')).map(
+    (harmony): Harmony => {
+      const title = getTextContent(harmony, 'h1#title1');
+      const titleNumber = getTextContent(harmony, 'p.title-number');
+      const rows = getHarmonyRow(harmony);
 
-function filterTextNodes(nodes: Node[]): Node[] {
-  return nodes.filter(
-    (node: Node): boolean => {
-      return node.nodeName.toLowerCase() !== '#text';
-    },
-  );
-}
-
-function getHarmonyVerses(nodes: Node[]): HarmonyVerse[] {
-  try {
-    const harmonyVerses: HarmonyVerse[] = [];
-    for (let x = 0; x < nodes.length; x += 2) {
-      // console.log(x);
-
-      const harmonyVerse = new HarmonyVerse();
-      harmonyVerse.verseRef = (nodes[x + 1] as Element).getAttribute(
-        'id',
-      ) as string;
-
-      harmonyVerse.verseTitle = (nodes[x] as Element).textContent as string;
-      harmonyVerses.push(harmonyVerse);
-    }
-    return harmonyVerses;
-  } catch (error) {
-    // console.log(`Node lengths ${nodes[0].nodeName}`);
-
-    throw error;
-  }
-}
-
-function getHarmonyCell(childNodes: Node[]): HarmonyCell {
-  const harmonyCell = new HarmonyCell();
-  const sortKeyElement = childNodes[0] as Element;
-  if (sortKeyElement.textContent) {
-    harmonyCell.sortKey = sortKeyElement.textContent;
-    childNodes.shift();
-  }
-  try {
-    if (childNodes[0].nodeName.toLowerCase() === 'sequence-title') {
-      harmonyCell.sequenceTitle = childNodes[0].textContent as string;
-      return harmonyCell;
-    }
-  } catch (error) {
-    return harmonyCell;
-  }
-  harmonyCell.harmonyVerses = getHarmonyVerses(childNodes);
-  return harmonyCell;
-}
-function getHarmonyXRefCell(childNodes: Node[]): HarmonyXRef[] {
-  const xRefCells = filterTextNodes(childNodes);
-
-  return xRefCells.map(
-    (xRefCell): HarmonyXRef => {
-      const harmonyXRefCell = new HarmonyXRef();
-      harmonyXRefCell.harmonyVerse = [];
-      const nodes = filterTextNodes(Array.from(xRefCell.childNodes));
-
-      harmonyXRefCell.xRefTitle =
-        nodes[0] && nodes[0].textContent ? nodes[0].textContent : '';
-
-      nodes.shift();
-      harmonyXRefCell.harmonyVerse = getHarmonyVerses(nodes);
-
-      return harmonyXRefCell;
+      return {
+        _id: harmony.id,
+        harmonyRows: rows,
+        title: title,
+        titleNumber: titleNumber,
+        _rev: undefined,
+      };
     },
   );
-
-  // return harmonyXRefCell;
 }
 
 async function main(): Promise<void> {
+  let c = 0;
   try {
-    (await getHarmoniesFileNames()).map(
-      async (fileName): Promise<void> => {
-        try {
-          const file = await readFile(fileName, { encoding: 'utf-8' });
-          const document = new JSDOM(file).window.document;
-
-          const rows = Array.from(document.querySelectorAll('tr'));
-          const columnLength = calculateColumnLength(rows);
-          // console.log(columnLength);
-
-          const rowsE = rows.map(
-            (row): (HarmonyCell | HarmonyXRef)[] => {
-              try {
-                const columns = Array.from(row.querySelectorAll('td')).splice(
-                  0,
-                  columnLength,
-                );
-                return columns.map(
-                  (
-                    column: HTMLTableDataCellElement,
-                  ): HarmonyCell | HarmonyXRef => {
-                    try {
-                      const childNodes = Array.from(column.childNodes).filter(
-                        (node: Node): boolean => {
-                          return node.nodeName.toLowerCase() !== '#text';
-                        },
-                      );
-
-                      if (childNodes[0]) {
-                        if (
-                          childNodes[0].nodeName.toLowerCase() === 'sortkey'
-                        ) {
-                          return getHarmonyCell(childNodes);
-                        } else if (
-                          childNodes[0].nodeName.toLowerCase() === 'xref'
-                        ) {
-                          try {
-                            const harmonyCell = new HarmonyCell();
-
-                            harmonyCell.harmonyXRef = getHarmonyXRefCell(
-                              childNodes,
-                            );
-                            // console.log(harmonyCell);
-
-                            return harmonyCell;
-                          } catch (error) {
-                            throw new Error('error in xref');
-                          }
-                        }
-                      }
-                      return new HarmonyCell();
-
-                      // if (childNodes.length % 2 !== 0) {
-                      //   console.log(childNodes.length);
-                      // }
-                    } catch (error) {
-                      console.log(error);
-
-                      throw new Error(
-                        'This document might not be a Harmony file',
-                      );
-                    }
-                  },
-                );
-              } catch (error) {
-                console.log(error);
-
-                throw error;
-              }
-            },
-          );
-          // rowsE.map(
-          //   (row): void => {
-          //     console.log(row.length);
-          //   },
-          // );
-
-          const harmony = new Harmony();
-          harmony._id = 'eng-harmony';
-          harmony.harmonyCells = rowsE;
-          // console.log(
-          //   normalize(
-          //     `../scripture_files/scriptures/harmony/${fileName
-          //       .replace('.html', '')
-          //       .replace(' ', '_')}.json`,
-          //   ),
-          // );
-          if (
-            !(await pathExists(
-              normalize('../scripture_files/scriptures/harmony'),
-            ))
-          ) {
-            console.log(
-              await mkdirp(normalize('../scripture_files/scriptures/harmony')),
-            );
-          }
-          await writeFile(
-            normalize(
-              `../scripture_files/scriptures/harmony/${basename(
-                fileName.toLowerCase(),
-              )
-                .replace('.html', '')
-                .replace(' ', '_')}.json`,
-            ),
-            JSON.stringify(harmony),
-          );
-
-          // console.log(results);
-
-          // rows.map(
-          //   (row): void => {
-          //     const cells = row.querySelectorAll('td');
-          //     if (cells.length !== 7) {
-          //       console.log(row.outerHTML);
-          //     }
-          //   },
-          // );
-        } catch (error) {}
-      },
-    );
+    await mkdirp(normalize('../scripture_files/scriptures/harmony'));
   } catch (error) {}
+  (await getHarmoniesFileNames()).map(
+    async (fileName): Promise<void> => {
+      try {
+        const file = await readFile(fileName);
+        const document = new JSDOM(file).window.document;
+
+        const harmonies = await getHarmonyChapters(document);
+        await console.log(harmonies);
+
+        await writeFile(
+          normalize(`../scripture_files/scriptures/harmony/harmony-${c}.json`),
+          JSON.stringify(harmonies),
+        );
+      } catch (error) {}
+    },
+  );
 }
 
 main();
