777d1714ed5f3c8802f9c25c0409469db0c7ab6a
Jared Parkinson <jared@parkinson.im>
2019-07-21 01:26:54 -0600

    bugs

--- OneInThineHand.org/src/app/components/offset-group/offset-group.component.ts
+++ OneInThineHand.org/src/app/components/offset-group/offset-group.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { OffsetGroup } from '../../services/offset-groups.service';
-import { VerseNote, Note } from '../../models/verse-notes';
+import {
+  VerseNote,
+  Note,
+  NoteGeography,
+  NotePronunciation,
+} from '../../models/verse-notes';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { GeoPopupComponent } from '../geo-popup/geo-popup.component';
+import { ProGuideComponent } from '../pro-guide/pro-guide.component';
 
 @Component({
   selector: 'app-offset-group',
@@ -8,9 +16,37 @@ import { VerseNote, Note } from '../../models/verse-notes';
   styleUrls: ['./offset-group.component.scss'],
 })
 export class OffsetGroupComponent implements OnInit {
+  public audio: HTMLAudioElement;
   @Input() public offsetGroup: OffsetGroup;
   @Input() public verseNotes: VerseNote;
-  constructor() {}
+  public constructor(public modalService: NgbModal) {}
+  public async geoClick(event: Event, note: NoteGeography): Promise<void> {
+    console.log('jhhh');
+
+    if ((event.target as HTMLElement).getAttribute('url')) {
+      const url = (event.target as HTMLElement).getAttribute('url') as string;
+      console.log(url);
+      // alert(url);
+      const result = await this.modalService.open(GeoPopupComponent, {
+        ariaLabelledBy: 'modal-basic-title',
+        backdropClass: 'add-notes-backdrop',
+        backdrop: true,
+      });
+
+      result.componentInstance.id = url;
+      // this.mod/ alService.open(ProGuideComponent);
+    }
+    //  else if (
+    // note.href &&
+    // (event.target as HTMLElement).classList.contains('note-category')
+    // )
+    // {
+    // console.log(note);
+    //
+    // this.audio = new Audio(`assets/audio/${note.href}`);
+    // this.audio.play();
+    // }
+  }
 
   public highlight(notes: Note[]): boolean {
     return (
@@ -25,5 +61,30 @@ export class OffsetGroupComponent implements OnInit {
 
   public ngOnInit() {
     // console.log(this.offsetGroup);
+  }
+  public async pronunciationClick(
+    event: Event,
+    note: NotePronunciation,
+  ): Promise<void> {
+    if ((event.target as HTMLElement).getAttribute('url')) {
+      const url = (event.target as HTMLElement).getAttribute('url') as string;
+      // alert(url);
+      const result = await this.modalService.open(ProGuideComponent, {
+        ariaLabelledBy: 'modal-basic-title',
+        backdropClass: 'add-notes-backdrop',
+        backdrop: true,
+      });
+
+      result.componentInstance.id = 'figure13_p21';
+      // this.mod/ alService.open(ProGuideComponent);
+    } else if (
+      note.href &&
+      (event.target as HTMLElement).classList.contains('note-category')
+    ) {
+      // console.log(note);
+
+      this.audio = new Audio(`assets/audio/${note.href}`);
+      this.audio.play();
+    }
   }
 }
